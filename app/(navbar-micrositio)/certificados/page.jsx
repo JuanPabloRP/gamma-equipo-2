e1fcb463d0a3f8f75b5bff8efdf28daa2c40adbf
Juan Pablo RP <jpabloroldanp@gmail.com>
2024-06-11 14:46:58 -0500

    arreglando error en Form.jsx

--- app/(navbar-micrositio)/certificados/page.jsx
+++ app/(navbar-micrositio)/certificados/page.jsx
@@ -1,24 +1,21 @@
-'use client'
+'use client';
 
-import Form from "@/components/certificados/Form/Form";
-import Historialdonaciones from "@/components/certificados/HistorialDonaciones/HistorialDonaciones";
-import Titulo from "@/components/certificados/Titulo/Titulo";
-import { ButtonDonar } from "@/components/paginaInicial/Button";
-import { useState } from "react";
-
-
+import Form from '@/components/certificados/Form/Form';
+import Historialdonaciones from '@/components/certificados/HistorialDonaciones/HistorialDonaciones';
+import Titulo from '@/components/certificados/Titulo/Titulo';
+import { ButtonDonar } from '@/components/paginaInicial/Button';
+import { useState } from 'react';
 
 const Certificados = () => {
-
-  const [registro, useRegistro] = useState(false);
-  return (
-    <div>
-      <Titulo />
-      <ButtonDonar />
-      <Form useRegistro={useRegistro}/>
-      <Historialdonaciones registro={registro}/>
-    </div>
-  );
+	const [registro, setRegistro] = useState(false);
+	return (
+		<div>
+			<Titulo />
+			<ButtonDonar />
+			<Form setRegistro={setRegistro} />
+			<Historialdonaciones registro={registro} />
+		</div>
+	);
 };
 
 export default Certificados;
